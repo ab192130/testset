e2eba1897bb95f8bb45bb02646d99b70a19861ca
ab192130 <ab192130@gmail.com>
2013-09-07 19:32:22 +0500

    +cookie sign in done +sign out button for own profile WIP

--- app.js
+++ app.js
@@ -1,121 +1,72 @@
 
-/**
- * Module dependencies.
- */
+    /**
+     * Module dependencies.
+     */
 
-var express = require('express');
-var routes = require('./routes');
-var user = require('./routes/user');
-var http = require('http');
-var path = require('path');
-var api = require('./models/api');
-var mongoose = require('mongoose');
-var Schema = mongoose.Schema;
+    var express = require('express');
+    var routes = require('./routes');
+    var user = require('./routes/user');
+    var http = require('http');
+    var path = require('path');
+    var api = require('./models/api');
+    var mongoose = require('mongoose');
+    var Schema = mongoose.Schema;
 
-var app = express();
+    var app = express();
 
-// all environments
-app.set('port', process.env.PORT || 3000);
-app.set('views', __dirname + '/views');
-app.set('view engine', 'jade');
-app.use(express.favicon());
-app.use(express.logger('dev'));
-app.use(express.bodyParser());
-app.use(express.methodOverride());
-app.use(app.router);
-app.use(express.static(path.join(__dirname, 'public')));
+    // all environments
+    app.set('port', process.env.PORT || 3000);
+    app.set('views', __dirname + '/views');
+    app.set('view engine', 'jade');
+    app.use(express.favicon());
+    app.use(express.logger('dev'));
+    app.use(express.cookieParser());
+    app.use(express.bodyParser());
+    app.use(express.methodOverride());
 
-// development only
-if ('development' == app.get('env')) {
-  app.use(express.errorHandler());
-}
+    app.use(express.static(path.join(__dirname, 'public')));
 
-http.createServer(app).listen(app.get('port'), function(){
-    console.log('Express server listening on port ' + app.get('port'));
-});
-
-//Database
-mongoose.connect('mongodb://localhost/proj01');
-var dbHashes = api.loadModels(mongoose);
-var UserSchema = new Schema(dbHashes.UserHash);
-var UserModel = mongoose.model('Users', UserSchema);
-
-
-app.get('/', routes.index);
-//app.get('/users', user.list);
-
-app.post('/user/new', function(req, res){
-
-    // Eger xanalar bos deyilse
-    if (req.body.login && req.body.pass){
-        // Find user
-        api.getUser(UserModel, req.body.login, function(err, data){
-            if (err) {throw err;} else {
-                if(data[0]){
-                    res.send('User already exists');
-//          console.log(data[0]);
-                } else {
-                    var newUser = new UserModel({name: req.body.login, pass: req.body.pass});
-                    newUser.save(function(err){
-                        if(err) throw err;
-//              res.render('./user/new', {login: req.body.login, pass: req.body.pass});
-                        res.redirect('/user/' + req.body.login);
-                        console.log('%s:%s', req.body.login, req.body.pass);
-                    });
-                }
-            }
-        });
-    } else {
-        res.send('Login or password is empty');
-    }
-});
-
-app.post('/user/login', function(req, res){
-
-    if (req.body.login && req.body.pass){
-        api.getUser(UserModel, req.body.login, function(err, data){
-            if (err) {throw err;} else {
-                if (!data[0]) {
-                    // Eger bele user movcud deyilse
-                    res.send('User not found!');
-                } else {
-                    //Eger bele user movcuddursa shifreni yoxlayirig
-                    if(req.body.pass == data[0].pass) {
-                        //Dogrudursa
-                        res.redirect('/user/' + req.body.login);
-                    } else {
-                        //Yanlisdirsa
-                        res.send('Invalid Password');
-                    }
-                }
-            }
-        });
-    } else {
-        res.send('Login or password is empty');
+    // development only
+    if ('development' == app.get('env')) {
+      app.use(express.errorHandler());
     }
 
-    //@TODO: login ve sifre yoxlanir ve sonra yonlenir userin profiline
-    //res.redirect('/user/' + req.body.login);
-});
-
-app.get('/user/:name', function(req, res){
-
-    // Find User
-    api.getUser(UserModel, req.params.name, function(err, data){
-        if (err) {throw err;} else {
-            if(data[0]){
-                res.render('./user/index', {login: data[0].name, pass: data[0].pass});
-                console.log('%s:%s', data[0].name, data[0].pass);
-                //        console.log('Requested User: ', data);
-            } else {
-                res.send('User Not Found!');
-            }
-        }
+    http.createServer(app).listen(app.get('port'), function(){
+        console.log('Express server listening on port ' + app.get('port'));
     });
-});
 
-app.get('/data', function(req, res){
-        UserModel.find({}, function(err,data){
-            res.json(data);
+    //Database
+    mongoose.connect('mongodb://localhost/proj01');
+    var dbHashes = api.loadModels(mongoose);
+    var UserSchema = new Schema(dbHashes.UserHash);
+    var UserModel = mongoose.model('Users', UserSchema);
+
+
+
+//    app.use(app.router);
+    app.get('/', routes.index);
+    //app.get('/users', user.list);
+
+    // User qeydiyyatdan kecir
+    app.post('/user/new', function(req, res){
+        api.supUser(req, res, api, UserModel);
+    });
+
+    // User sisteme daxil olur
+    app.post('/user/login', function(req, res){
+        api.sinUser(req, res, api, UserModel, function(uid){
+            res.cookie('uid', uid); //kukiye yazag
         });
-});
+
+    });
+
+    // Userin sehifesi goruntulenir
+    app.get('/user/:name', function(req, res){
+        api.openUser(req, res, api, UserModel)
+    });
+
+    app.get('/data', function(req, res){
+            UserModel.find({}, function(err,data){
+                res.json(data);
+            });
+    });
--- models/api.js
+++ models/api.js
@@ -1,18 +1,95 @@
-exports.loadModels = function ()
-{
 
-    var dbHashes = {
-        UserHash: {
-            name:{type: String},
-            pass:{type: String}
+    //load DB Hashes
+    exports.loadModels = function ()
+    {
+
+        var dbHashes = {
+            UserHash: {
+                name:{type: String},
+                pass:{type: String}
+            }
+        };
+
+        return dbHashes;
+    };
+
+    // Get User from DB
+    exports.getUser = function getUser(model, name, callback){
+        model.find({name: name}, function(err, data){
+            callback(err, data);
+        });
+    };
+
+
+    // SignUp User
+    exports.supUser = function(req, res, api, model){
+        // Eger xanalar bos deyilse
+        if (req.body.login && req.body.pass){
+            // Find user
+            api.getUser(model, req.body.login, function(err, data){
+                if (err) {throw err;} else {
+                    if(data[0]){
+                        res.send('User already exists');
+    //          console.log(data[0]);
+                    } else {
+                        var newUser = new model({name: req.body.login, pass: req.body.pass});
+                        newUser.save(function(err){
+                            if(err) throw err;
+                            res.redirect('/user/' + req.body.login);
+                            console.log('New user signed up %s:%s', req.body.login, req.body.pass);
+                        });
+                    }
+                }
+            });
+        } else {
+            res.send('Login or password is empty');
         }
     };
 
-    return dbHashes;
-};
+    // SignIn User
+    exports.sinUser = function(req, res, api, model, callback){
+        if (req.body.login && req.body.pass){
+            api.getUser(model, req.body.login, function(err, data){
+                if (err) {throw err;} else {
+                    if (!data[0]) {
+                        // Eger bele user movcud deyilse
+                        res.send('User not found!');
+                    } else {
+                        //Eger bele user movcuddursa shifreni yoxlayirig
+                        if(req.body.pass == data[0].pass) {
+                            //Dogrudursa
+                            callback(data[0]._id);
+                            res.redirect('/user/' + req.body.login);
 
-exports.getUser = function(model, name, callback){
-    model.find({name: name}, function(err, data){
-        callback(err, data);
-    });
-};
+
+                        } else {
+                            //Yanlisdirsa
+                            res.send('Invalid Password');
+                        }
+                    }
+                }
+            });
+        } else {
+            res.send('Login or password is empty');
+        }
+    };
+
+    exports.openUser = function(req, res, api, model){
+        api.getUser(model, req.params.name, function(err, data){
+            if (err) {throw err;} else {
+                if(data[0]){
+                    var own = false;
+                    if(data[0]._id == req.cookies.uid)
+                    {
+                        console.log('it is my profile');
+                        own = true;
+                    }
+                    res.render('./user/index', {login: data[0].name, pass: data[0].pass, own: own});
+                    console.log('%s:%s - %s', data[0].name, data[0].pass, data[0]._id);
+                } else {
+                    res.send('User Not Found!');
+                }
+            }
+        });
+    };
+
