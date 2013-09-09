fd375de8cb9c9c86c7b72fcaace6c964238f84ad
ab192130 <ab192130@gmail.com>
2013-09-09 20:29:17 +0500

    + header title added for all pages

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,7 @@
 exports.index = function(req, res){
     if (api.checkAuth(req.cookies)) {
         api.getUserById(req.cookies.uid, function(err, user){
-            res.render('index', {title: 'PROJ01', user: user});
+            res.render('index', {title: 'Project 01', user: user});
         });
     } else {
         res.render('index', {title: 'PROJ01'});
